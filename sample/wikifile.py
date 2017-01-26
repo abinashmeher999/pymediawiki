36f7910b6e110277f119920928e8b287999f5eeb
Ayushya Anand <ayuhsya@users.noreply.github.com>
2017-01-27 01:43:16 +0530

    Added get_fileusage (#43)
    * Added get_fileusage
    
    * Added fuprop again

--- sample/wikifile.py
+++ sample/wikifile.py
@@ -59,8 +59,42 @@ class WikiFile:
         self.payload['prop'] = None
         return df_list
 
+    def get_fileusage(self, fuprop="pageid|title|redirect", fulimit="max", funamespace="*", fushow="!redirect"):
+        prop = 'fileusage'
+        self.payload['prop'] = prop
+        self.payload['fuprop'] = fuprop
+        self.payload['fulimit'] = fulimit
+        self.payload['fushow'] = fushow
+        self.payload['funamespace'] = funamespace
+
+        res = requests.get(self.base_url, params=self.payload, headers=self.headers).json()
+
+        fu_list = {}
+        for page_id, page_content in res['query']['pages'].items():
+            if prop not in page_content:
+                continue
+            fu_list[page_content['title']] = page_content[prop]
+
+        while 'continue' in res:
+            self.payload['fucontinue'] = res['continue']['fucontinue']
+            res = requests.get(self.base_url, params=self.payload, headers=self.headers).json()
+            for page_id, page_content in res['query']['pages'].items():
+                if prop not in page_content:
+                    continue
+                if page_content['title'] not in fu_list:
+                    fu_list[page_content['title']] = []
+                fu_list[page_content['title']] += page_content[prop]
+
+        self.payload.pop('fuprop', None)
+        self.payload.pop('fulimit', None)
+        self.payload.pop('fushow', None)
+        self.payload.pop('funamespace', None)
+        self.payload.pop('fucontinue', None)
+        self.payload['prop'] = None
+        return fu_list
+
 if __name__ == "__main__":
-    titles = ['Image:1995.jpg', 'File:Albert Einstein Head.jpg', 'Image:Bombers.ogg']
+    titles = ['File:Example.jpg', 'File:Albert Einstein Head.jpg']
 
     try:
         wk = WikiFile(titles=titles)
@@ -70,3 +104,5 @@ if __name__ == "__main__":
 
     print("get_duplicatefiles: ")
     pprint(wk.get_duplicatefiles())
+    print("get_fileusage: ")
+    pprint(wk.get_fileusage())
